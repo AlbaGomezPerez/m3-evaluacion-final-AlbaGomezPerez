e3b49d39c6c51f3844e3a42e6437ff195cfde02c
Alba Gómez Pérez <albagope@gmail.com>
2019-09-24 12:30:47 +0200

    create GitHub Pages

---
+++ docs/precache-manifest.835f0df1a6493a63b56f575f69a86167.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "715390ca55a96f3da01cdcfff64f694d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3fedaac3a781dc8818ac",
+    "url": "./static/css/main.2003d6a5.chunk.css"
+  },
+  {
+    "revision": "4c5415eb985474421207",
+    "url": "./static/js/2.3ea5e3f4.chunk.js"
+  },
+  {
+    "revision": "3fedaac3a781dc8818ac",
+    "url": "./static/js/main.61adfb00.chunk.js"
+  },
+  {
+    "revision": "a1f774cd02a6463e78d6",
+    "url": "./static/js/runtime-main.d9bf1edf.js"
+  },
+  {
+    "revision": "75509f2ec9725b3cef07eefe651a4a8f",
+    "url": "./static/media/Rick.75509f2e.png"
+  }
+]);
