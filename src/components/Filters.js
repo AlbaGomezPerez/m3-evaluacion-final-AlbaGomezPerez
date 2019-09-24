40447d1571136272bf876e48d96a781a77f75114
Alba Gómez Pérez <albagope@gmail.com>
2019-09-24 11:20:17 +0200

    add input styles

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -5,8 +5,7 @@ const Filters = props => {
   const {SearchName, getNameInput} = props;
   return (
 	<div className="app__filters">
-		<p>Busca por nombre del personaje</p>
-		<input type="text" className="NameInput" onChange={getNameInput} value={SearchName}/>
+		<input type="text" className="NameInput" placeHolder="Write a character's name"onChange={getNameInput} value={SearchName}/>
 	</div>  
   );
 };
